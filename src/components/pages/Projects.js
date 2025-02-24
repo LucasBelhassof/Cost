8c5e7880ac0e91503aa09d8ff12cc7b6315907af
LucasBelhassof <lucassbleao@yahoo.com.br>
2025-02-24 17:16:55 -0300

    Quarta Versão

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,19 +1,27 @@
 import { useLocation } from 'react-router-dom'
 
-import Message from "../layout/Message.js"
+import Message from '../layout/Message.js'
+import styles from './Projects.module.css'
+import Container from '../layout/Container.js'
 
-function Projects(){
+function Projects() {
 
     const location = useLocation()
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
     }
 
     return (
         <div>
-            <h1>Meu Projetos</h1>
+            <div>
+                <h1>Meu Projetos</h1>
+                <a href="#">novo projeto</a>
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
